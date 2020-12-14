18aaae540455ca1d30161da61d7ff5b0ae2e3415
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-12-14 00:19:20 -0500

    set up all the methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
+
+    public NoNullArrayList() {
+        super();
+    }
+
+    public NoNullArrayList(int startingCapacity) {
+        super(startingCapacity);
+    }
+
+    public void add() {
+
+    }
+
+    public add(int index) {
+
+    }
+
+    public set(int index, int value) {
+        
+    }
+
+
 
 }
