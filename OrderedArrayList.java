99c91f813aa1e7f69d1abd3f35674cce23e01310
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-12-14 01:06:00 -0500

    credits to Reng's awesome tester

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -20,12 +20,16 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 
     public T set(int index, T element) {
         T remove = super.get(index);
+        if (element == null) throw new IllegalArgumentException("you cannot put null here");
+        else {
         super.remove(index);
         super.add(sortIndex(element), element);
+    }
         return remove;
     }
 
     private int sortIndex(T element) {
+        if(element == null) throw new IllegalArgumentException("cannot implement null"); 
         int index = 0;
         if (this.size() > 0){
             for (int i  = 0; i < super.size(); i++){
